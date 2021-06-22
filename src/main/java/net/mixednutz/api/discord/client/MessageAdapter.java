24c5c0d9be1cb60b3c95c1ae48a0da663eb8212f
apfesta <andy_festa@yahoo.com>
2021-06-21 22:41:46 -0400

    return value from post to timeline method

--- src/main/java/net/mixednutz/api/discord/client/MessageAdapter.java
+++ src/main/java/net/mixednutz/api/discord/client/MessageAdapter.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.javacord.api.entity.channel.ChannelType;
 import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.social.connect.ConnectionData;
@@ -14,6 +15,7 @@ import org.springframework.social.javacord.connect.DiscordConnectionFactory;
 
 import net.mixednutz.api.client.PostClient;
 import net.mixednutz.api.discord.model.DiscordChannel;
+import net.mixednutz.api.discord.model.MessageElement;
 import net.mixednutz.api.discord.model.MessageForm;
 
 public class MessageAdapter extends BaseDiscordAdapter implements PostClient<MessageForm> {
@@ -34,8 +36,8 @@ public class MessageAdapter extends BaseDiscordAdapter implements PostClient<Mes
 	}
 
 	@Override
-	public void postToTimeline(MessageForm post) {
-		acceptWithApi((api)->{	
+	public MessageElement postToTimeline(MessageForm post) {
+		return applyWithApi((api)->{	
 			long channelId = post.getChannelId()!=null ? post.getChannelId() : 
 				post.getChannelIdAsString()!=null ? Long.parseLong(post.getChannelIdAsString()) : 
 						defaultChannelId;
@@ -43,17 +45,17 @@ public class MessageAdapter extends BaseDiscordAdapter implements PostClient<Mes
 					channelId)
 					.orElseThrow(()->new IllegalArgumentException("Channel "+channelId+" not found"));
 			try {
-				channel.asTextChannel().get().sendMessage(post.toContent())
-					.thenAccept((msg)->{
-						log.info("Sent message: ID:{}; Timestamp:{}; Channel:{}; Content:{}", 
-								msg.getIdAsString(), 
-								msg.getCreationTimestamp(),
-								msg.getChannel().getIdAsString(),
-								msg.getContent());
-					}).get();
+				Message msg = channel.asTextChannel().get().sendMessage(post.toContent()).get();
+				log.info("Sent message: ID:{}; Timestamp:{}; Channel:{}; Content:{}", 
+						msg.getIdAsString(), 
+						msg.getCreationTimestamp(),
+						msg.getChannel().getIdAsString(),
+						msg.getContent());
+				return new MessageElement(msg);
 			} catch (Exception e) {
 				log.error("Error sending message to Discord", e);
 			} 
+			return null;
 		});
 	}
 	
